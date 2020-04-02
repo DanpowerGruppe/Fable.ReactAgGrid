cda35831323257cbbc20d82f22ec11f7560f127c
ButA1 <alexanderbut97@gmail.com>
2020-04-02 14:12:35 +0200

    pls kill me

--- app/AgGrid/View.fs
+++ app/AgGrid/View.fs
@@ -2,20 +2,24 @@ module Components.AgGrid.View
 
 open Components.AgGrid.Types
 
+open Fable.Core
 open Fable.React
 open Fable.React.Props  
 open Fable.ReactAgGrid    
 open Fable.Core.JsInterop 
  
+// printfn "%A" (createObj [ "font-size" ==> "16px" ])
+
 let agGrid (tableRep :TableRep) dispatch =
-    div [ Class "ag-theme-balham"; Style [ Height "400px"; Width "800px"  ] ] [
+    div [ Class "ag-theme-balham"; Style [ Height "400px"; CSSProp.Width "800px"  ] ] [
         grid [
-            ColumnDefs [|
-                yield {ColumnDef.Create ("Datum") ("date") with editable = false; pinned = "left"}
-                for i,header in tableRep.HeadCol |> Array.indexed ->
-                    {ColumnDef.Create (header) (string i)
-                        with onCellValueChanged = (fun ev -> dispatch (SetGridInput ({Row = int ev.node.id; Col = int ev.colDef.field; Value = string ev.newValue})))}
-            |]
+            // ColumnDefs [|                
+            //     yield {ColumnDef.Create ("Datum") ("date") with editable = false; pinned = "left"}
+            //     for i,header in tableRep.HeadCol |> Array.indexed ->
+            //         {ColumnDef.Create (header) (string i)
+            //             with onCellValueChanged = (fun ev -> dispatch (SetGridInput ({Row = int ev.node.id; Col = int ev.colDef.field; Value = string ev.newValue})))}
+            // |]
+            ColumnDefs [| ColumnDef [HeaderName "Datum"; Field "date"] |]                                
             RowData (tableRep.Grid |> Array.map box)
             RowHeight 36.
             RowStyle (createObj [ "font-size" ==> "16px" ])
@@ -28,3 +32,5 @@ let render (state: State) dispatch =
         [ p [ ] [ str "Basic AgGrid " ]
           agGrid state.TableRep dispatch
           br [ ]]
+
+          
--- src/Fable.ReactAgGrid.fs
+++ src/Fable.ReactAgGrid.fs
@@ -22,44 +22,86 @@ type Event =
       column: Column
       node : RowNode }
 
-type ColumnDef =
-    { headerName : string
-      columnGroupShow : bool
-      headerClass : obj
-      suppressColumnsToolPanel: bool
-      field : string
-      sortable : bool
-      filter : bool;
-      checkboxSelection : bool
-      editable : bool
-      resizable : bool
-      pinned : string
-      cellStyle : obj
-      onCellValueChanged : Event -> unit }
-    static member Create headerName field =
-        {   headerName = headerName;
-            columnGroupShow = false
-            headerClass = createObj[] 
-            suppressColumnsToolPanel = false
-            field = field;
-            sortable = false;
-            filter = false;
-            checkboxSelection = false;
-            editable = true;
-            pinned = ""
-            resizable = true;
-            cellStyle = createObj[] 
-            onCellValueChanged =
-                (fun x -> printfn "Column %s Row %s Value %A" x.colDef.field x.node.id x.newValue ) }
+// type ColumnDef =
+//     { headerName : string
+//       columnGroupShow : bool
+//       headerClass : obj
+//       suppressColumnsToolPanel: bool
+//       field : string
+//       sortable : bool
+//       filter : bool;
+//       checkboxSelection : bool
+//       editable : bool
+//       resizable : bool
+//       pinned : string
+//       cellStyle : obj
+//       width : string option
+//       onCellValueChanged : Event -> unit }
+//     static member Create headerName field =
+//         {   headerName = headerName;
+//             columnGroupShow = false
+//             headerClass = createObj[] 
+//             suppressColumnsToolPanel = false
+//             field = field;
+//             sortable = false;
+//             filter = false;
+//             checkboxSelection = false;
+//             editable = true;
+//             pinned = ""
+//             resizable = true;
+//             cellStyle = createObj[] 
+//             width = None
+//             onCellValueChanged =
+//                 (fun x -> printfn "Column %s Row %s Value %A" x.colDef.field x.node.id x.newValue ) }
 
 
-type GridProps =
-    | ColumnDefs of ColumnDef array
+
+type ColumnDefOptions = 
+    | HeaderName of string
+    | ColumnGroupShow of bool
+    | HeaderClass of obj
+    | SuppressColumnsToolPanel of bool
+    | Field of string
+    | Sortable of bool
+    | Filter of bool
+    | CheckboxSelection of bool
+    | Editable of bool
+    | Resizable of bool
+    | Pinned of string
+    | CellStyle of obj
+    | Width of string
+    | OnCellValueChanged of (Event -> unit)
+    // member this.LowerFirstString = 
+    //     match this with
+    //     | HeaderName _ -> "headerName"
+    //     | ColumnGroupShow _ -> "columnGroupShow"
+    //     | HeaderClass _ -> "headerClass"
+    //     | SuppressColumnsToolPanel _ -> "suppressColumnsToolPanel"
+    //     | Field _ -> "field"
+    //     | Sortable _ -> "sortable"
+    //     | Filter _ -> "filter"
+    //     | CheckboxSelection _ -> "checkboxSelection"
+    //     | Editable _ -> "editable"
+    //     | Resizable _ -> "pinned"
+    //     | Pinned _ -> "resizable"
+    //     | CellStyle _ -> "cellStyle"
+    //     | Width _ -> "width"
+    //     | OnCellValueChanged _ -> "onCellValueChanged"
+
+let ColumnDef options = 
+    keyValueList CaseRules.LowerFirst options
+
+type GridOptions =
+    | ColumnDefs of obj array 
     | RowData of obj array
     | RowHeight of float
     | HeaderHeight of float
     | RowStyle of obj
     | StopEditingWhenGridLosesFocus of bool
 
-let inline grid (props : GridProps list) (elems : ReactElement list) : ReactElement =
+  
+
+let inline grid (props : GridOptions list) (elems : ReactElement list) : ReactElement =
+    let test = keyValueList CaseRules.LowerFirst props  
+    printfn "%A" test
     ofImport "AgGridReact" "ag-grid-react" (keyValueList CaseRules.LowerFirst props) elems
